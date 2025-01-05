831f1bcbfe68e2fe72f9b254023ce67bb7f03843
varsharani123777 <varsharani9279@gmail.com>
2025-01-05 17:59:07 +0000

    container configure done

--- .devcontainer/basics/variable.js
+++ .devcontainer/basics/variable.js
@@ -10,6 +10,7 @@ studPassword="4567"
 studcity="delhi"
 /*
 prefer not to use var
+because of issue in block scope and functional scope
 */
 console.log(Studentid);
 console.table([studPassword,studentEmail,studcity,studstate]);
