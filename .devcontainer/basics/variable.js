01aa32c1b0880ce363e85ee97a2ddb52b2773451
varsharani123777 <varsharani9279@gmail.com>
2025-01-05 17:49:02 +0000

    container configure done

---
+++ .devcontainer/basics/variable.js
@@ -0,0 +1,15 @@
+const Studentid = 12345
+let studentEmail ="vvarshhrani876@gmail.com"
+var studPassword= "765"
+studcity ="Agra"
+let studstate;
+
+// studentid = 87//not allowed
+studentEmail="hgsr@gmail.com"
+studPassword="4567"
+studcity="delhi"
+/*
+prefer not to use var
+*/
+console.log(Studentid);
+console.table([studPassword,studentEmail,studcity,studstate]);
