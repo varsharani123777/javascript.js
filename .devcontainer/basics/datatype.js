4e5ae1e205f072c6b560df1bedecb49af465cd4d
varsharani123777 <varsharani9279@gmail.com>
2025-01-05 18:18:20 +0000

    container configure done

---
+++ .devcontainer/basics/datatype.js
@@ -0,0 +1,25 @@
+"use strict";//treat all JS code as newer version
+
+//alert(3 + 2) //we are using nodejs,not browser
+
+console.log(3 + 
+    7 )
+
+//code readability should be high
+console.log("varshu")
+let name="rani"
+let age= 17
+let isLoggedIn= false
+
+//number=> 2 to power 53
+//bigint 
+//string=>""
+//boolean
+//null=>standalone value
+//undefined=>
+    //sysbol=>unique
+//object
+console.log(typeof age); 
+console.log(typeof undefined);//underfined
+ console.log(typeof null);//object
+ 
