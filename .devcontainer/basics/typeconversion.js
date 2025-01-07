e8e7a9993cfc3bf8caf1e3f2ca62bf23ed14e8b7
varsharani123777 <varsharani9279@gmail.com>
2025-01-07 15:27:03 +0000

    container configure done

---
+++ .devcontainer/basics/typeconversion.js
@@ -0,0 +1,21 @@
+let score=44
+console.log(typeof score);
+console.log(typeof (score));
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+/*
+"33"=>33
+"44abc"=>nan
+true=>1; false=>0
+*/
+let isloggedIn = 1
+let booleanisloggedin= Boolean(isloggedIn);
+console.log(booleanisloggedin);
+// 1=> true; 0=>false
+//""=>false
+//"hit"=>false
+let someno=78
+let stringnum=String(someno);
+console.log(typeof someno);
+console.log(stringnum);
