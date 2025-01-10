2018eb5fc88912f57f994a64869b8fbe4e38eb0d
varsharani123777 <varsharani9279@gmail.com>
2025-01-10 14:48:36 +0000

    container configure done

---
+++ .devcontainer/basics/compareop.js
@@ -0,0 +1,16 @@
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2>=1);
+// console.log(26<=18);
+// console.log(21!=45);
+// console.log("2">56);
+
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+console.log(undefined==0);
+//===
+console.log("4"===4);
---
+++ .devcontainer/basics/datatypesummary.js
@@ -0,0 +1,20 @@
+//primitive
+
+//7type: string,number,boolean,null,undefined,symbol(make a unique value in advanced javascript)
+//BigInt
+
+const score=100
+const scorevalue= 100.6
+const isloggedIn = false
+const outsidetemp =nul
+let useremail;
+
+const id= Symbol('1234')
+const anotherId = Symbol('1234')
+console.log(id===anotherId);
+// bigint use
+const bignumber = 987654323456789n
+
+
+//non-primitve(refrence)
+// Array,objects,functions
